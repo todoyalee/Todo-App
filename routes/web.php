475d875434e4fbefdd039a74511148f0f01fa263
Thorfin <m.a.belkouri@gmail.com>
2024-02-01 10:00:59 +0100

    CreatingOurFirstTestRoute

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/todos/index', [App\Http\Controllers\TodoController::class, 'index'])->name('index');
